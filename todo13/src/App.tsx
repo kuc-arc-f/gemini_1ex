46f67ecccb227f633eb1f747b4926f792196f7f4
中島浩二 <knaka0209@gmail.com>
2024-12-14 11:51:52 +0900

    up, 1214a3

--- todo13/src/App.tsx
+++ todo13/src/App.tsx
@@ -2,9 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 
 //
 import Home from './client/home';
-//import Workers1 from './client/workers1';
 import Workers2 from './client/workers2';
 import Workers3 from './client/workers3';
+import Todo11 from './client/todo11';
 import Todo12 from './client/todo12';
 import Todo13 from './client/todo13';
 
@@ -18,6 +18,7 @@ export default function App(){
         <Route path="/" element={<Home />} />
         <Route path="/workers2" element={<Workers2 />} />
         <Route path="/workers3" element={<Workers3 />} />
+        <Route path="/todo11" element={<Todo11 />} />
         <Route path="/todo12" element={<Todo12 />} />
         <Route path="/todo13" element={<Todo13 />} />
       </Routes>
